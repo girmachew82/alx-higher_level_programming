2d438464e9c689fd7a3930e56ea425b0449cfd4d
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2022-09-09 00:31:36 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -18,8 +18,6 @@
 --
 -- Table structure for table `tv_genres`
 --
-CREATE DATABASE hbtn_0d_tvshows;
-use hbtn_0d_tvshows;
 DROP TABLE IF EXISTS `tv_genres`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
