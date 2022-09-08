8f4e000e35ace24498748251b7c87e957699bf90
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2022-09-09 00:45:31 +0300

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
-WHERE tv_shows.id = tv_shows_genres.genre_id ORDER BY tv_shows_title, tv_show_genres.genre_id ASC;
+WHERE tv_shows.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
